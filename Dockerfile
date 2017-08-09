d18e3d4279a50873786ec436d870835b048fda8a
Владимир <vladimir@MacBook-Pro-Vladimir.local>
2017-08-09 18:07:05 +0300

    Building in docker container

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM starefossen/node-imagemagick
+
+RUN npm i -g grunt-cli phantomjs2
---
+++ build.sh
@@ -0,0 +1,5 @@
+npm i
+grunt test
+npm run styl 
+grunt compile image copy_previews image_resize 
+cp -R css output
